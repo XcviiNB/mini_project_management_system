b8b7b1f3cb6f39683d5ee37e0257263a03c2b901
XcviiNB <nathanielbatausafgo@gmail.com>
2025-10-09 09:32:56 +0800

    added api

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\TaskCommentController;
+use App\Http\Controllers\Api\TaskController;
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('/users', UserController::class);
+
+Route::apiResource('/projects', ProjectController::class);
+
+Route::apiResource('/tasks', TaskController::class);
+
+Route::apiResource('/task-comments', TaskCommentController::class);
